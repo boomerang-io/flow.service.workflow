25ec1ac9243edf68a24a410104a8c50331930a10
amhudson <amhudson@us.ibm.com>
2020-08-07 15:07:30 -0500

    creating Flow global settings

---
+++ src/main/java/net/boomerangplatform/controller/ConfigurationController.java
@@ -0,0 +1,34 @@
+package net.boomerangplatform.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import net.boomerangplatform.model.FlowSettings;
+import net.boomerangplatform.service.crud.ConfigurationService;
+
+
+
+@RestController
+@RequestMapping("/ci/settings/config")
+@Secured({"ROLE_admin"})
+public class ConfigurationController {
+
+  @Autowired
+  private ConfigurationService configurationService;
+
+  @Secured({"ROLE_operator", "ROLE_admin"})
+  @GetMapping(value = "")
+  public List<FlowSettings> getAppConfiguration() {
+    return configurationService.getAllSettings();
+  }
+
+  @PutMapping(value = "")
+  public List<FlowSettings> updateSettings(@RequestBody List<FlowSettings> settings) {
+    return configurationService.updateSettings(settings);
+  }
+}
