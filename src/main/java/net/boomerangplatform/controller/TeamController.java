37b9c5cf7fc15eb1e42d857ed36473f0ad1bc025
Marcus Roy <marcus.d.roy@gmail.com>
2020-08-10 11:34:24 -0500

    :zap: moving team get listing to be in both stand alone and core mode (#6)

--- src/main/java/net/boomerangplatform/controller/TeamController.java
+++ src/main/java/net/boomerangplatform/controller/TeamController.java
@@ -2,7 +2,12 @@ package net.boomerangplatform.controller;
 
 import java.util.LinkedList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,9 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import net.boomerangplatform.model.CreateFlowTeam;
+import net.boomerangplatform.model.TeamQueryResult;
 import net.boomerangplatform.model.TeamWorkflowSummary;
+import net.boomerangplatform.model.profile.SortSummary;
 import net.boomerangplatform.mongo.entity.FlowTeamConfiguration;
 import net.boomerangplatform.mongo.entity.FlowUserEntity;
 import net.boomerangplatform.mongo.model.UserType;
@@ -28,7 +36,7 @@ public class TeamController {
 
   @Autowired
   private UserIdentityService userIdentityService;
-
+  
   @PostMapping(value = "/teams")
   public void createCiTeam(@RequestBody CreateFlowTeam createCiTeamRequest) {
     flowTeamService.createFlowTeam(createCiTeamRequest.getCreatedGroupId());
@@ -69,4 +77,33 @@ public class TeamController {
     return flowTeamService.createNewTeamProperty(teamId, property);
   }
 
+  
+  @GetMapping(value = "/manage/teams")
+  public TeamQueryResult getTeams(@RequestParam(required = false) String query,
+      @RequestParam(defaultValue = "ASC") Direction order,
+      @RequestParam(required = false) String sort, @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "100") int size) {
+
+    SortSummary sortSummary = new SortSummary();
+    sortSummary.setProperty("name");
+    sortSummary.setDirection(Direction.ASC.toString());
+
+    Sort pagingSort = Sort.by(Direction.ASC, "name");
+
+    if (StringUtils.isNotBlank(sort)) {
+      Direction direction = order == null ? Direction.ASC : order;
+      sortSummary.setDirection(direction.toString());
+      sortSummary.setProperty(sort);
+
+      pagingSort = Sort.by(direction, sort);
+    }
+
+    final Pageable pageable = PageRequest.of(page, size, pagingSort);
+
+    TeamQueryResult response = flowTeamService.getAllAdminTeams(pageable);
+    response.setupSortSummary(sortSummary);
+    return response;
+  }
+  
+  
 }
