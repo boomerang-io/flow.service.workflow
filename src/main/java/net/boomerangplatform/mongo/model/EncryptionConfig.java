6cbae4d56419fde9cbdf3f89dafae71a8ac2470b
amhudson <amhudson@us.ibm.com>
2020-08-13 08:50:23 -0500

    update

--- src/main/java/net/boomerangplatform/mongo/model/EncryptionConfig.java
+++ src/main/java/net/boomerangplatform/mongo/model/EncryptionConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:encryption.properties")
+@PropertySource("classpath:application.properties")
 public class EncryptionConfig {
 
   @Value("${mongo.encrypt.secret:secret}")
