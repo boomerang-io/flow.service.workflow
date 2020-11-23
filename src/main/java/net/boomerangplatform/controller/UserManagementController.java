5721c09ee48124a091613409ebdf9a5f9fd6b40b
amhudson <amhudson@us.ibm.com>
2020-11-23 17:38:29 -0600

    remove annotation

--- src/main/java/net/boomerangplatform/controller/UserManagementController.java
+++ src/main/java/net/boomerangplatform/controller/UserManagementController.java
@@ -1,10 +1,8 @@
 package net.boomerangplatform.controller;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConditionalOnProperty(value = "flow.mode", havingValue = "standalone", matchIfMissing = false)
 public class UserManagementController {
 
 
