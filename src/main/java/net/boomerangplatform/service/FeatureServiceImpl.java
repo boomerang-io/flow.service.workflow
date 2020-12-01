c51392ecb724e279b93e5359296019553b73045e
Adrienne Hudson <amhudson@us.ibm.com>
2020-12-01 10:12:59 -0600

    add feature flags (#36)
    * add feature flags
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update for flow external navigation
    
    * update
    
    * removing boomerang.activation flag
    
    * update

--- src/main/java/net/boomerangplatform/service/FeatureServiceImpl.java
+++ src/main/java/net/boomerangplatform/service/FeatureServiceImpl.java
@@ -3,6 +3,7 @@ package net.boomerangplatform.service;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import net.boomerangplatform.model.FlowFeatures;
 import net.boomerangplatform.mongo.model.Config;
@@ -14,6 +15,40 @@ public class FeatureServiceImpl implements FeatureService {
   private static final String VERIFIED_TASK_EDIT_KEY = "enable.verified.tasks.edit";
   @Autowired
   private FlowSettingsService settingsService;
+  
+  @Value("${flow.feature.workflow.quotas}")
+  private boolean flowFeatureWorkflowQuotas;
+  
+  @Value("${flow.feature.workflow.triggers}")
+  private boolean flowFeatureWorkflowTriggers;
+  
+  @Value("${flow.feature.workflow.tokens}")
+  private boolean flowFeatureWorkflowTokens;
+  
+  @Value("${flow.feature.team.properties}")
+  private boolean flowFeatureTeamProperties;
+  
+  @Value("${flow.feature.global.properties}")
+  private boolean flowFeatureGlobalProperties;
+  
+  @Value("${flow.feature.team.management}")
+  private boolean flowFeatureTeamManagement;
+  
+  @Value("${flow.feature.user.management}")
+  private boolean flowFeatureUserManagement;
+  
+  @Value("${flow.feature.taskManager}")
+  private boolean flowFeatureTaskManager;
+  
+  @Value("${flow.feature.settings}")
+  private boolean flowFeatureSettings;
+  
+  @Value("${flow.feature.activity}")
+  private boolean flowFeatureActivity;
+  
+  @Value("${flow.feature.insights}")
+  private boolean flowFeatureInsights;
+  
 
   @Override
   public FlowFeatures getFlowFeatures() {
@@ -28,8 +63,19 @@ public class FeatureServiceImpl implements FeatureService {
     else {
       features.put(VERIFIED_TASK_EDIT_KEY, false);
     }
+    features.put("workflow.quotas", flowFeatureWorkflowQuotas);
+    features.put("workflow.triggers", flowFeatureWorkflowTriggers);
+    features.put("workflow.tokens", flowFeatureWorkflowTokens);
+    features.put("team.properties", flowFeatureTeamProperties);
+    features.put("global.properties", flowFeatureGlobalProperties);
+    features.put("team.management", flowFeatureTeamManagement);
+    features.put("user.management", flowFeatureUserManagement);
+    features.put("taskManager", flowFeatureTaskManager);
+    features.put("settings", flowFeatureSettings);
+    features.put("activity",flowFeatureActivity);
+    features.put("insights",flowFeatureInsights);
     
-    flowFeatures.setFeatures(features);
+    flowFeatures.setFeatures(features); 
     return flowFeatures;
   }
 
---
+++ src/main/java/net/boomerangplatform/service/crud/NavigationServiceImpl.java
@@ -0,0 +1,135 @@
+package net.boomerangplatform.service.crud;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import net.boomerangplatform.model.Navigation;
+import net.boomerangplatform.model.NavigationType;
+import net.boomerangplatform.security.service.ApiTokenService;
+
+@Service
+public class NavigationServiceImpl implements NavigationService {
+
+  @Value("${flow.externalUrl.navigation}")
+  private String flowExternalUrlNavigation;
+
+  @Autowired
+  private ApiTokenService apiTokenService;
+
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String TOKEN_PREFIX = "Bearer ";
+
+  @Autowired
+  @Qualifier("internalRestTemplate")
+  private RestTemplate restTemplate;
+
+  @Override
+  public List<Navigation> getNavigation(boolean isUserAdmin) {
+
+    if (!flowExternalUrlNavigation.isBlank()) {
+      List<Navigation> response = new ArrayList<>();
+      Navigation workflows = new Navigation();
+      workflows.setName("Workflows");
+      workflows.setType(NavigationType.link);
+      workflows.setIcon("FlowData16");
+      workflows.setLink("/workflows");
+      response.add(workflows);
+
+      Navigation activity = new Navigation();
+      activity.setName("Activity");
+      activity.setType(NavigationType.link);
+      activity.setIcon("Activity16");
+      activity.setLink("/activity");
+      response.add(activity);
+
+      Navigation insights = new Navigation();
+      insights.setName("Insights");
+      insights.setType(NavigationType.link);
+      insights.setIcon("ChartScatter16");
+      insights.setLink("/insights");
+      response.add(insights);
+
+      Navigation management = new Navigation();
+      management.setName("Management");
+      management.setIcon("SettingsAdjust16");
+      management.setChildLinks(new ArrayList<>());
+      management.setType(NavigationType.category);
+
+      Navigation teamProperties = new Navigation();
+      teamProperties.setName("Team Properties");
+      teamProperties.setLink("/team-properties");
+      teamProperties.setType(NavigationType.link);
+      management.getChildLinks().add(teamProperties);
+      response.add(management);
+
+      if (isUserAdmin) {
+        Navigation admin = new Navigation();
+        admin.setName("Admin");
+        admin.setType(NavigationType.category);
+        admin.setIcon("Settings16");
+        admin.setChildLinks(new ArrayList<>());
+
+        Navigation teams = new Navigation();
+        teams.setName("Teams");
+        teams.setLink("/admin/teams");
+        teams.setType(NavigationType.link);
+        admin.getChildLinks().add(teams);
+
+        Navigation users = new Navigation();
+        users.setName("Users");
+        users.setLink("/admin/users");
+        users.setType(NavigationType.link);
+        admin.getChildLinks().add(users);
+
+        Navigation properties = new Navigation();
+        properties.setName("Properties");
+        properties.setLink("/admin/properties");
+        properties.setType(NavigationType.link);
+        admin.getChildLinks().add(properties);
+
+        Navigation quotas = new Navigation();
+        quotas.setName("Quotas");
+        quotas.setLink("/admin/quotas");
+        quotas.setType(NavigationType.link);
+        admin.getChildLinks().add(quotas);
+
+        Navigation settings = new Navigation();
+        settings.setName("Settings");
+        settings.setLink("/admin/settings");
+        settings.setType(NavigationType.link);
+        admin.getChildLinks().add(settings);
+
+        Navigation taskManager = new Navigation();
+        taskManager.setName("Task Manager");
+        taskManager.setLink("/admin/task-templates");
+        taskManager.setType(NavigationType.link);
+        admin.getChildLinks().add(taskManager);
+
+        response.add(admin);
+
+      }
+
+      return response;
+    }
+
+    else {
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.add(AUTHORIZATION_HEADER, TOKEN_PREFIX + apiTokenService.getUserToken());
+
+      HttpEntity<String> request = new HttpEntity<>(headers);
+      ResponseEntity<List<Navigation>> response = restTemplate.exchange(flowExternalUrlNavigation,
+          HttpMethod.GET, request, new ParameterizedTypeReference<List<Navigation>>() {});
+      return response.getBody();
+    }
+  }
+}
