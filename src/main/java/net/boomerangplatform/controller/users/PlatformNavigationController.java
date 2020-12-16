d348ba44a98bf5d68e25bf6a5350f97a27ca317d
Marcus Roy <marcus.d.roy@gmail.com>
2020-12-16 10:58:47 -0600

    Adding new external url override for platform navigation

--- src/main/java/net/boomerangplatform/controller/users/PlatformNavigationController.java
+++ src/main/java/net/boomerangplatform/controller/users/PlatformNavigationController.java
@@ -1,7 +1,6 @@
 package net.boomerangplatform.controller.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,7 @@ import net.boomerangplatform.service.profile.LaunchpadNavigationService;
 
 @RestController
 @RequestMapping("/workflow/users/navigation")
-public class UsersNavigationController {
+public class PlatformNavigationController {
 
   @Autowired
   private LaunchpadNavigationService launchpadNavigationService;
--- src/main/java/net/boomerangplatform/service/profile/LaunchpadNavigationServiceImpl.java
+++ src/main/java/net/boomerangplatform/service/profile/LaunchpadNavigationServiceImpl.java
@@ -2,12 +2,23 @@ package net.boomerangplatform.service.profile;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import net.boomerangplatform.model.profile.Features;
 import net.boomerangplatform.model.profile.Navigation;
 import net.boomerangplatform.model.profile.NavigationResponse;
 import net.boomerangplatform.model.profile.Platform;
+import net.boomerangplatform.security.service.ApiTokenService;
 
 @Service
 public class LaunchpadNavigationServiceImpl implements LaunchpadNavigationService {
@@ -32,10 +43,31 @@ public class LaunchpadNavigationServiceImpl implements LaunchpadNavigationServic
 
   @Value("${boomerang.baseUrl}")
   private String platformBaseUrl;
+  
+  @Autowired
+  @Qualifier("internalRestTemplate")
+  private RestTemplate restTemplate;
+  
+  @Value("${flow.externalUrl.platformNavigation}")
+  private String platformNavigationUrl;
+  
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String TOKEN_PREFIX = "Bearer ";
 
+  @Autowired
+  private ApiTokenService apiTokenService;
+  
   @Override
   public NavigationResponse getLaunchpadNavigation(boolean isUserAdmin) {
+    if (platformNavigationUrl.isBlank()) {
+      return getFlowNavigationResponse();
+    }
+    else {
+      return getExternalNavigationResponse();
+    }
+  }
 
+  private NavigationResponse getFlowNavigationResponse() {
     final List<Navigation> navList = new ArrayList<>();
     NavigationResponse navigationResponse = new NavigationResponse();
     navigationResponse.setNavigation(navList);
@@ -59,5 +91,25 @@ public class LaunchpadNavigationServiceImpl implements LaunchpadNavigationServic
     navigationResponse.setPlatform(platform);
 
     return navigationResponse;
+  }
+  
+  private NavigationResponse getExternalNavigationResponse() {
+    UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(platformNavigationUrl).build();
+    HttpHeaders headers = buildHeaders();
+  
+    HttpEntity<String> requestUpdate = new HttpEntity<>("", headers);
+    ResponseEntity<NavigationResponse> response =
+        restTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, requestUpdate, NavigationResponse.class);
+    return response.getBody();
+  }
+  
+  private HttpHeaders buildHeaders() {
+
+    final HttpHeaders headers = new HttpHeaders();
+    headers.add("Accept", "application/json");
+    headers.add(AUTHORIZATION_HEADER, TOKEN_PREFIX + apiTokenService.getUserToken());
+
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    return headers;
   }
 }
