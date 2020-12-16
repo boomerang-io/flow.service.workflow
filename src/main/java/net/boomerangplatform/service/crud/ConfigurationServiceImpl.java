bf4324720851a170b8ee11c13a5dc9e60eba95b8
Marcus Roy <marcus.d.roy@gmail.com>
2020-12-16 17:12:22 -0600

    :fire: refactoring to allow internal settings to be retrieved

--- src/main/java/net/boomerangplatform/service/crud/ConfigurationServiceImpl.java
+++ src/main/java/net/boomerangplatform/service/crud/ConfigurationServiceImpl.java
@@ -5,19 +5,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import net.boomerangplatform.model.FlowSettings;
 import net.boomerangplatform.mongo.entity.FlowSettingsEntity;
-import net.boomerangplatform.mongo.entity.FlowUserEntity;
 import net.boomerangplatform.mongo.model.Config;
 import net.boomerangplatform.mongo.model.ConfigurationType;
-import net.boomerangplatform.mongo.model.UserType;
 import net.boomerangplatform.mongo.service.FlowSettingsService;
-import net.boomerangplatform.mongo.service.FlowUserService;
-import net.boomerangplatform.security.service.UserDetailsService;
-import net.boomerangplatform.service.UserIdentityService;
 import net.boomerangplatform.util.DateUtil;
 
 @Service
@@ -26,18 +19,9 @@ public class ConfigurationServiceImpl implements ConfigurationService {
   @Autowired
   private FlowSettingsService serviceSettings;
 
-  @Autowired
-  private UserDetailsService userDetailsService;
-
-  @Autowired
-  private FlowUserService userService;
-
-  @Autowired
-  UserIdentityService service;
-
   @Override
   public List<FlowSettings> getAllSettings() {
-    validateUser();
+  
 
     final List<FlowSettings> settingList = new LinkedList<>();
     final List<FlowSettingsEntity> entityList = serviceSettings.getAllConfigurations();
@@ -51,7 +35,6 @@ public class ConfigurationServiceImpl implements ConfigurationService {
 
   @Override
   public List<FlowSettings> updateSettings(List<FlowSettings> settings) {
-    validateUser();
     for (final FlowSettings setting : settings) {
       final FlowSettingsEntity entity = serviceSettings.getConfigurationById(setting.getId());
       if (entity.getType() == ConfigurationType.ValuesList) {
@@ -65,15 +48,6 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     return this.getAllSettings();
   }
 
-  protected void validateUser() {
-
-    FlowUserEntity userEntity = service.getCurrentUser();
-    if (userEntity == null || (!userEntity.getType().equals(UserType.admin)
-        && !userEntity.getType().equals(UserType.operator))) {
-
-      throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
-    }
-  }
 
   private void setConfigsValue(final FlowSettings setting, final FlowSettingsEntity entity) {
     for (final Config config : setting.getConfig()) {
