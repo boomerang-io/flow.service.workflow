d1b319877e53c2a780ab6f9705f1b0dd0cf71051
Marcus Roy <marcus.d.roy@gmail.com>
2020-12-17 22:47:25 -0600

    :fire: Updating workflow service to pass through platform urls

--- src/main/java/net/boomerangplatform/model/profile/Platform.java
+++ src/main/java/net/boomerangplatform/model/profile/Platform.java
@@ -5,11 +5,15 @@ public class Platform {
   private String version;
   private String name;
   private String signOutUrl;
+  private String communityUrl;
   private String platformName;
   private Boolean displayLogo;
   private Boolean privateTeams;
   private Boolean sendMail;
 
+  private String baseServicesUrl;
+  private String baseEnvUrl;
+  
   public String getVersion() {
     return version;
   }
@@ -32,6 +36,14 @@ public class Platform {
 
   public void setSignOutUrl(String signOutUrl) {
     this.signOutUrl = signOutUrl;
+  }
+
+  public String getCommunityUrl() {
+    return communityUrl;
+  }
+
+  public void setCommunityUrl(String communityUrl) {
+    this.communityUrl = communityUrl;
   }
 
   public String getPlatformName() {
@@ -64,6 +76,22 @@ public class Platform {
 
   public void setSendMail(Boolean sendMail) {
     this.sendMail = sendMail;
+  }
+
+  public String getBaseServicesUrl() {
+    return baseServicesUrl;
+  }
+
+  public void setBaseServicesUrl(String baseServicesUrl) {
+    this.baseServicesUrl = baseServicesUrl;
+  }
+
+  public String getBaseEnvUrl() {
+    return baseEnvUrl;
+  }
+
+  public void setBaseEnvUrl(String baseEnvUrl) {
+    this.baseEnvUrl = baseEnvUrl;
   }
 
 }
