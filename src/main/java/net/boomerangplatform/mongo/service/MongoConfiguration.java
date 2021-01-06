e2c6d7956d8ebaf4e19880510ce08f3cd50a9211
Marcus Roy <marcus.d.roy@gmail.com>
2021-01-06 15:04:18 -0600

    Removing mongo logging for collection name lookups

--- src/main/java/net/boomerangplatform/mongo/service/MongoConfiguration.java
+++ src/main/java/net/boomerangplatform/mongo/service/MongoConfiguration.java
@@ -1,7 +1,5 @@
 package net.boomerangplatform.mongo.service;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,19 +9,12 @@ public class MongoConfiguration {
   @Value("${flow.mongo.collection.prefix}")
   private String workflowCollectionPrefix;
   
-  private static final Logger LOGGER = LogManager.getLogger();
-  
   public String fullCollectionName(String collectionName) {
     
-    LOGGER.info("Looking up collection name: " + collectionName);
-    
     if (workflowCollectionPrefix == null || workflowCollectionPrefix.isBlank()) {
-      LOGGER.info("Detected blank collection name, returning no prefix");
       return "" + collectionName;
     }
     String newCollectionName = workflowCollectionPrefix + "_" + collectionName;
-    
-    LOGGER.info("Returning: " + newCollectionName);
     
     return newCollectionName;
   }
