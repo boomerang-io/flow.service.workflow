a85aa56812c6cad078b928c01e26e4311daa4b09
Adrienne Hudson <amhudson@us.ibm.com>
2021-01-29 16:34:19 -0600

    Fix activity filter (#73)
    * fix activity filter
    
    * fix filter

--- src/main/java/net/boomerangplatform/mongo/service/FlowWorkflowServiceImpl.java
+++ src/main/java/net/boomerangplatform/mongo/service/FlowWorkflowServiceImpl.java
@@ -1,7 +1,6 @@
 package net.boomerangplatform.mongo.service;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import net.boomerangplatform.mongo.entity.WorkflowEntity;
@@ -63,26 +62,6 @@ public class FlowWorkflowServiceImpl implements FlowWorkflowService {
   public List<WorkflowEntity> getAllWorkflows() {
     return workFlowRepository.findAll();
   }
-
-  @Override
-  public List<WorkflowEntity> getSystemWorkflows(Optional<List<String>> statuses,
-      Optional<List<String>> triggers) {
-    if (statuses.isPresent() && triggers.isEmpty()) {
-
-      return workFlowRepository.findByScopeAndStatusIn(WorkflowScope.system, statuses.get());
-    } else if (triggers.isPresent() && statuses.isEmpty()) {
-
-      return workFlowRepository.findByScopeAndTriggersIn(WorkflowScope.system, triggers.get());
-    } else if (triggers.isPresent() && statuses.isPresent()) {
-
-      return workFlowRepository.findByScopeAndStatusInAndTriggersIn(WorkflowScope.system,
-          statuses.get(), triggers.get());
-    } else {
-      return workFlowRepository.findByScope(WorkflowScope.system);
-    }
-
-  }
-
 
   @Override
   public List<WorkflowEntity> getSystemWorkflows() {
