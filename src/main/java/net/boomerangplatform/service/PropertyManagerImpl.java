70617bd4d26f7a966a8313fc33d74b7b506283a1
Marcus Roy <marcus.d.roy@gmail.com>
2021-02-12 03:09:27 -0600

    Making property replacement generic

--- src/main/java/net/boomerangplatform/service/PropertyManagerImpl.java
+++ src/main/java/net/boomerangplatform/service/PropertyManagerImpl.java
@@ -422,14 +422,14 @@ public class PropertyManagerImpl implements PropertyManager {
 
   private String getEncodedPropertiesForMap(Map<String, String> map) {
     
-    if (map.containsKey("version-name")) {
-      map.put("version.name", map.get("version-name"));
+    for (Map.Entry<String,String> entry : map.entrySet()) {
+      String originalKey = entry.getKey();
+      String value = entry.getValue();
+  
+      String modifiedKey = originalKey.replaceAll("-", "\\.");
+      map.put(modifiedKey, value);
     }
-
-    if (map.containsKey("docker-image-name")) {
-      map.put("docker.image.name", map.get("docker-image-name"));
-    }
-    
+      
     try {
       Properties properties = new Properties();
       properties.putAll(map);
