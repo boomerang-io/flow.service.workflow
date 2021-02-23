e1e437d1d16c446817b4b80cb353598030cd0ad4
Marcus Roy <marcus.d.roy@gmail.com>
2021-02-23 17:41:36 -0600

    Adding initial support for run workflow

--- src/main/java/net/boomerangplatform/service/refactor/TaskClient.java
+++ src/main/java/net/boomerangplatform/service/refactor/TaskClient.java
@@ -1,5 +1,8 @@
 package net.boomerangplatform.service.refactor;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import net.boomerangplatform.model.FlowWebhookResponse;
+import net.boomerangplatform.model.RequestFlowExecution;
 import net.boomerangplatform.mongo.model.internal.InternalTaskRequest;
 import net.boomerangplatform.mongo.model.internal.InternalTaskResponse;
 
@@ -7,5 +10,5 @@ public interface TaskClient {
   
   public void startTask(InternalTaskRequest taskRequest);  
   public void endTask(InternalTaskResponse taskResponse);
-
+  public FlowWebhookResponse submitWebhookEvent(@RequestBody RequestFlowExecution request);
 }
