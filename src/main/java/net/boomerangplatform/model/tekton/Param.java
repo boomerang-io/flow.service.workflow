33afc4453e302e3c0b0fdfb03175d64036ae6cca
Marcus Roy <marcus.d.roy@gmail.com>
2021-03-03 16:12:33 -0600

    Removing start marker for yaml and parsing fixes

--- src/main/java/net/boomerangplatform/model/tekton/Param.java
+++ src/main/java/net/boomerangplatform/model/tekton/Param.java
@@ -1,37 +1,50 @@
 package net.boomerangplatform.model.tekton;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-public class Param{
-    private String name;
-    private String type;
-    private String description;
-    
-    @JsonProperty("default")
-    private String defaultString;
-    
-    public String getDefaultString() {
-      return defaultString;
-    }
-    public void setDefaultString(String defaultString) {
-      this.defaultString = defaultString;
-    }
-    public String getName() {
-      return name;
-    }
-    public void setName(String name) {
-      this.name = name;
-    }
-    public String getType() {
-      return type;
-    }
-    public void setType(String type) {
-      this.type = type;
-    }
-    public String getDescription() {
-      return description;
-    }
-    public void setDescription(String description) {
-      this.description = description;
-    }
+public class Param {
+  private String name;
+  private String type;
+  private String description;
+  
+  private Map<String, Object> unknownFields = new HashMap<>();
+
+  @JsonAnyGetter
+  @JsonPropertyOrder(alphabetic = true)
+  public Map<String, Object> otherFields() {
+    return unknownFields;
+  }
+
+  @JsonAnySetter
+  public void setOtherField(String name, Object value) {
+    unknownFields.put(name, value);
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
