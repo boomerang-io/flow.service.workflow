636fe3c0843460d6de56273597c8657b5272f903
Marcus Roy <marcus.d.roy@gmail.com>
2021-03-03 14:53:26 -0600

    :fire: adding missing fields for yaml export

--- src/main/java/net/boomerangplatform/model/tekton/Annotations.java
+++ src/main/java/net/boomerangplatform/model/tekton/Annotations.java
@@ -2,13 +2,15 @@ package net.boomerangplatform.model.tekton;
 import java.util.HashMap;
 import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter; 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder; 
 
 public class Annotations{
   
   private Map<String, Object> unknownFields = new HashMap<>();
 
   @JsonAnyGetter
+  @JsonPropertyOrder(alphabetic=true)
   public Map<String, Object> otherFields() {
       return unknownFields;
   }
