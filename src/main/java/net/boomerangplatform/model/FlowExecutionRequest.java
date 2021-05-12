371ee084d7fa75ffeba5238edad4e7623911b552
Marcus Roy <marcus.d.roy@gmail.com>
2021-05-12 15:28:36 -0500

    Updates to support latest changes for tekton alignment

--- src/main/java/net/boomerangplatform/model/FlowExecutionRequest.java
+++ src/main/java/net/boomerangplatform/model/FlowExecutionRequest.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class FlowExecutionRequest {
 
+  private boolean applyQuotas;
+  
   private Map<String, String> properties;
 
   public Map<String, String> getProperties() {
@@ -12,6 +14,14 @@ public class FlowExecutionRequest {
 
   public void setProperties(Map<String, String> properties) {
     this.properties = properties;
+  }
+
+  public boolean isApplyQuotas() {
+    return applyQuotas;
+  }
+
+  public void setApplyQuotas(boolean applyQuotas) {
+    this.applyQuotas = applyQuotas;
   }
 
 }
