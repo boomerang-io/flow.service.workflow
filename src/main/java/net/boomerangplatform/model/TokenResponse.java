0383b8f4d1b4379955fa4aaf3d7d3b24a6f710a2
Marcus Roy <marcus.d.roy@gmail.com>
2021-07-30 12:17:32 -0500

    :fire: Adding token management end points

---
+++ src/main/java/net/boomerangplatform/model/TokenResponse.java
@@ -0,0 +1,14 @@
+package net.boomerangplatform.model;
+
+public class TokenResponse extends Token {
+  private String tokenValue;
+
+  public String getTokenValue() {
+    return tokenValue;
+  }
+
+  public void setTokenValue(String tokenValue) {
+    this.tokenValue = tokenValue;
+  }
+
+}
--- src/main/java/net/boomerangplatform/mongo/service/FlowTokenServiceImpl.java
+++ src/main/java/net/boomerangplatform/mongo/service/FlowTokenServiceImpl.java
@@ -4,13 +4,19 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import net.boomerangplatform.model.Token;
+import net.boomerangplatform.model.TokenResponse;
+import net.boomerangplatform.mongo.entity.FlowUserEntity;
 import net.boomerangplatform.mongo.entity.TokenEntity;
 import net.boomerangplatform.mongo.model.TokenScope;
 import net.boomerangplatform.mongo.repository.FlowTokenRepository;
+import net.boomerangplatform.service.UserIdentityService;
 
 @Service
 public class FlowTokenServiceImpl implements FlowTokenService {
@@ -18,52 +24,101 @@ public class FlowTokenServiceImpl implements FlowTokenService {
   @Autowired
   private FlowTokenRepository tokenRepository;
 
+  @Autowired
+  private UserIdentityService userService;
+  
   @Override
-  public List<TokenEntity> findAllGlobalTokens() {
-    // TODO Auto-generated method stub
-    return null;
+  public List<Token> findAllGlobalTokens() {
+    List<TokenEntity> tokens = tokenRepository.findByScope(TokenScope.global);
+    return buildTokenList(tokens);
+  }
+  
+  @Override
+  public List<Token> findAllTeamTokens(String teamId) {
+    List<TokenEntity> tokens = tokenRepository.findByScopeAndTeamId(TokenScope.team, teamId);
+    return buildTokenList(tokens);
   }
 
-  @Override
-  public List<TokenEntity> findAllTeamTokens(String teamId) {
-    // TODO Auto-generated method stub
-    return null;
+  private List<Token> buildTokenList(List<TokenEntity> tokens) {
+    List<Token> tokenList = new LinkedList<>();
+    for (TokenEntity te : tokens) {
+      String creatorId = te.getCreatorId();
+      Token token = convertEntityToToken(te, creatorId);
+      tokenList.add(token);
+    }
+    return tokenList;
+  }
+
+  private Token convertEntityToToken(TokenEntity te, String creatorId) {
+    Token token = new Token();
+    token.setId(te.getId());
+    token.setDescription(te.getDescription());
+    token.setExpiryDate(te.getExpiryDate());
+    token.setCreationDate(te.getCreationDate());
+    token.setCreatorId(creatorId);
+    FlowUserEntity user = userService.getUserByID(creatorId); 
+    if (user != null) {
+      token.setCreatorName(user.getName());
+    }
+    Date currentDate = new Date();
+    if (token.getExpiryDate() != null && token.getExpiryDate().getTime () < currentDate.getTime()) {
+       token.setExpired(true);
+    }
+    return token;
   }
 
   @Override
   public void deleteToken(String tokenId) {
-    // TODO Auto-generated method stub
-
+    this.tokenRepository.deleteById(tokenId);
   }
 
   @Override
-  public String createTeamToken(String teamId, Date expiryDate, String description) {
-
-    TokenEntity tokenEntity = createNewToken(expiryDate, description, TokenScope.team);
-    tokenEntity.setScopeId(teamId);
+  public TokenResponse createTeamToken(String teamId, Date expiryDate, String description) {
+    FlowUserEntity currentUser = userService.getCurrentUser();
+    String creatorId = currentUser.getId();
+    TokenEntity tokenEntity = createNewToken(expiryDate, description, TokenScope.team, creatorId);
+    tokenEntity.setTeamId(teamId);
+    
     String prefix = getPrefixForScope(TokenScope.team);
     String uniqueToken = prefix + "_" + UUID.randomUUID().toString().toLowerCase();
     final String hashToken = hashString(uniqueToken);
     tokenEntity.setToken(hashToken);
-    return uniqueToken;
+  
+    this.tokenRepository.save(tokenEntity);
+    Token token = convertEntityToToken(tokenEntity, creatorId);
+    TokenResponse response = new TokenResponse();
+    BeanUtils.copyProperties(token, response);
+    response.setTokenValue(uniqueToken);
+    return response;
   }
 
   @Override
-  public String createSystemToken(Date expiryDate, String description) {
-    TokenEntity tokenEntity = createNewToken(expiryDate, description, TokenScope.global);
+  public TokenResponse createSystemToken(Date expiryDate, String description) {
+    FlowUserEntity currentUser = userService.getCurrentUser();
+    String creatorId = currentUser.getId();
+    TokenEntity tokenEntity = createNewToken(expiryDate, description, TokenScope.global, creatorId);
     String prefix = getPrefixForScope(TokenScope.global);
     String uniqueToken = prefix + "_" + UUID.randomUUID().toString();
     final String hashToken = hashString(uniqueToken);
     tokenEntity.setToken(hashToken);
-    return uniqueToken;
+    
+    this.tokenRepository.save(tokenEntity);
+    Token token = convertEntityToToken(tokenEntity, creatorId);
+    TokenResponse response = new TokenResponse();
+    response.setTokenValue(uniqueToken);
+    BeanUtils.copyProperties(token, response);
+    
+    return response;
   }
 
-  private TokenEntity createNewToken(Date expiryDate, String description, TokenScope scope) {
+  private TokenEntity createNewToken(Date expiryDate, String description, TokenScope scope, String creatorId) {
     TokenEntity tokenEntity = new TokenEntity();
     tokenEntity.setCreationDate(new Date());
     tokenEntity.setExpiryDate(expiryDate);
     tokenEntity.setDescription(description);
     tokenEntity.setScope(scope);
+    tokenEntity.setCreatorId(creatorId);
+    
     return tokenEntity;
   }
 
