72b1c128cf1d151436bfacdbbb7213686a958821
Iulian Corcoja <iulian@corcoja.com>
2021-11-05 18:24:30 +0200

    Minor cleanup + upgrade lib eventing to `0.2.2`

--- src/main/java/io/boomerang/Application.java
+++ src/main/java/io/boomerang/Application.java
@@ -2,6 +2,7 @@ package io.boomerang;
 
 import java.time.Clock;
 import java.util.concurrent.Executor;
+import com.github.alturkovic.lock.mongo.configuration.EnableMongoDistributedLock;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import com.github.alturkovic.lock.mongo.configuration.EnableMongoDistributedLock;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -22,15 +22,12 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @EnableAsync(proxyTargetClass = true)
 @EnableMongoDistributedLock
 @SecurityScheme(type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER,
-name = "serviceToken", description = "x-access-token")
-@OpenAPIDefinition(info = @Info(title = "Boomerang Flow", version = "3.4",
-description = ""
-))
+    name = "serviceToken", description = "x-access-token")
+@OpenAPIDefinition(info = @Info(title = "Boomerang Flow", version = "3.4", description = ""))
 public class Application {
 
-
   private static final Logger LOGGER = LogManager.getLogger();
-  
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
@@ -39,12 +36,12 @@ public class Application {
   public Clock clock() {
     return Clock.systemDefaultZone();
   }
-  
+
   @Bean(name = "flowAsyncExecutor")
   public Executor getCiExecutor() {
     int maxThreads = 200;
     int maxQueue = 100000;
-    
+
     LOGGER.info("Creating task executor service: (max concurrent threads: %d) (max queue: %d)",
         maxThreads, maxQueue);
 
@@ -57,5 +54,4 @@ public class Application {
     executor.initialize();
     return executor;
   }
-
 }
