08a0a2238d1c5033b6d4fd5f4a091365c9e898c1
amhudson <amhudson@us.ibm.com>
2022-01-12 15:37:10 -0600

    debug user workflow duration

--- src/main/java/io/boomerang/service/crud/FlowActivityServiceImpl.java
+++ src/main/java/io/boomerang/service/crud/FlowActivityServiceImpl.java
@@ -96,7 +96,7 @@ public class FlowActivityServiceImpl implements FlowActivityService {
   @Autowired
   private FlowSettingsService flowSettingsService;
 
-  
+
   @Autowired
   private FlowWorkflowActivityService flowActivityService;
 
@@ -904,7 +904,7 @@ public class FlowActivityServiceImpl implements FlowActivityService {
     if (error != null) {
       activity.setError(error);
     }
-    
+
     flowActivityService.saveWorkflowActivity(activity);
 
     String workflowId = activity.getWorkflowId();
@@ -1001,11 +1001,16 @@ public class FlowActivityServiceImpl implements FlowActivityService {
     }
     
     long maxDuration = TimeUnit.MINUTES.toMillis(this.maxWorkflowDuration);
+    System.out.println("********max duration app.prop***** "+ maxDuration);
     if (scope == WorkflowScope.user) {
       maxDuration = Integer.parseInt(flowSettingsService.getConfiguration("users", "max.user.workflow.duration").getValue());
+      System.out.println("********usermax duration***** "+ maxDuration);
     } else if (scope == WorkflowScope.team) {
       /** Retrieve from settings. */
+      System.out.println("*******team*max duration***** "+ maxDuration);
     }
+    
+    System.out.println("********max duration***** "+ maxDuration);
     
     List<TaskExecutionEntity> activites = taskService.findTaskActiivtyForActivity(activityId);
 
@@ -1025,6 +1030,9 @@ public class FlowActivityServiceImpl implements FlowActivityService {
       }
     }
 
+    System.out.println("********total duration***** "+ totalDuration);
+    
+    System.out.println("********maxDuration < totalDuration***** "+ (maxDuration < totalDuration);
     if (maxDuration < totalDuration) {
       return true;
     }
