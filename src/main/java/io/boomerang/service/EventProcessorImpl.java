f606642eeee7b9d92cdd1cebf9678e3dd68e8c43
amhudson <amhudson@us.ibm.com>
2022-11-14 20:16:49 -0600

    fix workflow password parameters

--- src/main/java/io/boomerang/service/EventProcessorImpl.java
+++ src/main/java/io/boomerang/service/EventProcessorImpl.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.Optional;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
@@ -30,11 +31,14 @@ import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import io.boomerang.model.FlowActivity;
 import io.boomerang.model.FlowExecutionRequest;
 import io.boomerang.model.eventing.EventResponse;
+import io.boomerang.mongo.entity.WorkflowEntity;
 import io.boomerang.mongo.model.KeyValuePair;
 import io.boomerang.mongo.model.Triggers;
 import io.boomerang.mongo.model.WorkflowProperty;
 import io.boomerang.service.crud.WorkflowService;
 import io.boomerang.service.refactor.TaskService;
+import io.boomerang.util.ParameterMapper;
+import io.boomerang.util.DataAdapterUtil.FieldType;
 import io.cloudevents.CloudEvent;
 import io.cloudevents.v1.AttributesImpl;
 import io.cloudevents.v1.CloudEventBuilder;
@@ -200,7 +204,7 @@ public class EventProcessorImpl implements EventProcessor {
       property.setKey("eventId");
       property.setValue(event.getAttributes().getId());
       cloudEventLabels.add(property);
-      //executionRequest.setLabels(cloudEventLabels);
+      // executionRequest.setLabels(cloudEventLabels);
       executionRequest.setProperties(processProperties(eventData, workflowId));
 
       FlowActivity activity = executionService.executeWorkflow(workflowId, Optional.of(trigger),
@@ -277,7 +281,22 @@ public class EventProcessorImpl implements EventProcessor {
     ObjectMapper mapper = new ObjectMapper();
     Map<String, String> payloadProperties =
         mapper.convertValue(eventData, new TypeReference<Map<String, String>>() {});
-    properties.putAll(payloadProperties);
+
+    List<KeyValuePair> propertyList = ParameterMapper.mapToKeyValuePairList(payloadProperties);
+    if (payloadProperties != null) {
+      WorkflowEntity workflow = workflowService.getWorkflow(workflowId);
+      Map<String, WorkflowProperty> workflowPropMap = workflow.getProperties().stream().collect(
+          Collectors.toMap(WorkflowProperty::getKey, WorkflowProperty -> WorkflowProperty));
+      // Use default value for password-type parameter when user input value is null when executing
+      // workflow.
+      propertyList.stream().forEach(p -> {
+        if (workflowPropMap.get(p.getKey()) != null
+            && FieldType.PASSWORD.value().equals(workflowPropMap.get(p.getKey()).getType())
+            && p.getValue() == null) {
+          p.setValue(workflowPropMap.get(p.getKey()).getDefaultValue());
+        }
+      });
+    }
 
     // properties.put("eventPayload", eventData.toString());
 
@@ -285,7 +304,8 @@ public class EventProcessorImpl implements EventProcessor {
       logger.info("processProperties() - " + k + "=" + v);
     });
 
-    return properties;
+
+    return mapper.convertValue(propertyList, new TypeReference<Map<String, String>>() {});
   }
 
   private Boolean isTriggerEnabled(String trigger, String workflowId, String topic) {
