4dbb6508cc21aaa92d370555ecc33705b6fb8e89
andrei_olariu_ro <andrei.olariu@ro.ibm.com>
2022-11-23 17:18:10 +0200

    This is split from Eventing Branch, with general fixes, not directly related to Eventing

--- src/main/java/io/boomerang/exceptions/RunWorkflowException.java
+++ src/main/java/io/boomerang/exceptions/RunWorkflowException.java
@@ -3,12 +3,4 @@ package io.boomerang.exceptions;
 @SuppressWarnings("serial")
 public class RunWorkflowException extends RuntimeException {
 
-  public RunWorkflowException(String message) {
-    super(message);
-  }
-
-  public RunWorkflowException() {
-    super();
-  }
-
 }
