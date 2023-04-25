0c948c023f65dcf35c8434a0257a8811b4fd438b
Tyson Lawrie <tyson@lawrie.com.au>
2023-04-25 12:23:51 +1000

    :fire: WIP - removed legacy execution

--- src/main/java/io/boomerang/service/FlowExecutionService.java
+++
@@ -1,7 +0,0 @@
-package io.boomerang.service;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface FlowExecutionService {
-  CompletableFuture<Boolean> executeWorkflowVersion(String workFlowId, String activityId);
-}
