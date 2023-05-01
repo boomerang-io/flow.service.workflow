f0dcda8e0f09de87045a2d05abfc53bbe3564c4a
Tyson Lawrie <tlawrie@users.noreply.github.com>
2023-05-01 17:15:25 +1000

    WIP - Auth

---
+++ src/main/java/io/boomerang/v4/controller/InternalDebugController.java
@@ -0,0 +1,45 @@
+package io.boomerang.v4.controller;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.boomerang.security.model.CreateTokenRequest;
+import io.boomerang.security.model.CreateTokenResponse;
+import io.boomerang.security.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/internal/debug")
+@ConditionalOnProperty(name = "flow.debug", havingValue = "true")
+public class InternalDebugController {
+
+  @Autowired
+  private TokenService tokenService;
+
+  @PostMapping("/token")
+  @Operation(summary = "Create Token")
+  public CreateTokenResponse create(@Valid @RequestBody CreateTokenRequest newToken) {
+    return tokenService.create(newToken);
+  }
+
+//  @GetMapping("token-scopes")
+//  public List<TokenTypeResponse> getTokenScopes() {
+//
+//    List<TokenTypeResponse>  responses = new LinkedList<>();
+//
+//    for (TokenScope type : TokenScope.values()) {
+//      TokenTypeResponse response = new TokenTypeResponse();
+//      response.setAccess(type.access());
+//      response.setObject(type.object());
+//      response.setTypes(type.types());
+//
+//      responses.add(response);
+//
+//    }
+//    return responses;
+//  }
+}
