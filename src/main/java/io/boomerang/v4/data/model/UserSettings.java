41c2fdc249631ef45c175a9b5229a65a22a22071
Tyson Lawrie <tlawrie@users.noreply.github.com>
2023-05-10 15:39:42 +1000

    Removed additional properties from UserSettings

--- src/main/java/io/boomerang/v4/data/model/UserSettings.java
+++ src/main/java/io/boomerang/v4/data/model/UserSettings.java
@@ -1,19 +1,11 @@
 package io.boomerang.v4.data.model;
 
-import java.util.HashMap;
-import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 // Future extensibility
 public class UserSettings {
 
   private Boolean isFirstVisit = true;
   private Boolean isShowHelp = true;
   private Boolean hasConsented = false;
-  @JsonIgnore
-  private final Map<String, Object> additionalProperties = new HashMap<>();
 
   public Boolean getIsFirstVisit() {
     return isFirstVisit;
@@ -32,13 +24,5 @@ public class UserSettings {
   }
   public void setHasConsented(Boolean hasConsented) {
     this.hasConsented = hasConsented;
-  }
-  @JsonAnyGetter
-  public Map<String, Object> getAdditionalProperties() {
-    return this.additionalProperties;
-  }
-  @JsonAnySetter
-  public void setAdditionalProperty(String name, Object value) {
-    this.additionalProperties.put(name, value);
   }
 }
