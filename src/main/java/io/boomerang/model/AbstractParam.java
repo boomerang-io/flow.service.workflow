f07f8a3e67638a279b7035b87e435020819a8beb
Tyson Lawrie <tlawrie@users.noreply.github.com>
2023-10-04 10:20:37 +1100

    Return only NON_NULL for AbstractParam

--- src/main/java/io/boomerang/model/AbstractParam.java
+++ src/main/java/io/boomerang/model/AbstractParam.java
@@ -3,9 +3,12 @@ package io.boomerang.model;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
 public class AbstractParam {
 
   private String key;
