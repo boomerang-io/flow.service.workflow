51f5e58efa50eacd9bc5be0b440a9e240c438922
Tyson Lawrie <tlawrie@users.noreply.github.com>
2023-10-19 15:34:12 +1100

    WIP - Integrations

---
+++ src/main/java/io/boomerang/integrations/data/entity/IntegrationTemplatesEntity.java
@@ -0,0 +1,76 @@
+package io.boomerang.integrations.data.entity;
+
+import java.net.URI;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+@Document(collection = "#{@mongoConfiguration.fullCollectionName('integration_templates')}")
+public class IntegrationTemplatesEntity {
+  @Id
+  private String id;
+
+  private String type;
+  
+  private URI url;
+  
+  private String status;
+  
+  private String description;
+
+  private String instructions;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public URI getUrl() {
+    return url;
+  }
+
+  public void setUrl(URI url) {
+    this.url = url;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getInstructions() {
+    return instructions;
+  }
+
+  public void setInstructions(String instructions) {
+    this.instructions = instructions;
+  }
+  
+  
+}
