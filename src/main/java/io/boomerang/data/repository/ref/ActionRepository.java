c6d85b37145b94ed0bf97814672fd2e714ce739f
Tyson Lawrie <tlawrie@users.noreply.github.com>
2024-05-03 13:25:22 +1000

    fix: cancel Actions when WorkflowRun is cancelled

--- src/main/java/io/boomerang/data/repository/ref/ActionRepository.java
+++ src/main/java/io/boomerang/data/repository/ref/ActionRepository.java
@@ -2,14 +2,11 @@ package io.boomerang.data.repository.ref;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
 import io.boomerang.data.entity.ref.ActionEntity;
-import io.boomerang.model.enums.RelationshipType;
 import io.boomerang.model.enums.ref.ActionStatus;
 import io.boomerang.model.enums.ref.ActionType;
 
@@ -36,7 +33,9 @@ public interface ActionRepository extends MongoRepository<ActionEntity, String> 
   
   void deleteByWorkflowRef(String workflowRef);
   
-  @Query("{'workflowRunRef': ?0")
+  void deleteByWorkflowRunRef(String workflowRunRef);
+  
+  @Query("{'workflowRunRef': ?0 }")
   @Update("{ '$set' : { 'status' : ?1 } }")
   long updateStatusByWorkflowRunRef(String ref, ActionStatus status);
 }
